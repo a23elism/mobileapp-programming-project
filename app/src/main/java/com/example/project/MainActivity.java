aac063c80c2698bc35d999001a4c16e3c07ca31c
a23elism <a23elism@student.his.se>
2024-05-22 22:28:04 +0200

    Added functionality to Adapter + implementation in MainActivity (also internet access)

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,49 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a23elism";
+
+    ArrayList<Color> Colors= new ArrayList<>();
+    Gson gson = new Gson();
+
+    String json = gson.toJson(Colors);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        new JsonTask(this).execute(JSON_URL);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+        Type type = new TypeToken<ArrayList<Color>>() {}.getType();
+        ArrayList<Color> Colors = gson.fromJson(json, type);
+
+        RecyclerView RecyclerView = findViewById(R.id.recyclerView);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter (this, Colors);
+        RecyclerView.setAdapter(adapter);
+        RecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
     }
 
 }
--- app/src/main/java/com/example/project/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/project/RecyclerViewAdapter.java
@@ -1,4 +1,50 @@
 package com.example.project;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    Context context;
+    ArrayList <Color> Colors;
+    public RecyclerViewAdapter(Context context, ArrayList<Color> Colors){
+        this.context =context;
+        this.Colors=Colors;
+    }
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view =inflater.inflate(R.layout.recycler_layout, parent, false);
+
+        return new RecyclerViewAdapter.MyViewHolder (view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.textview.setText(Colors.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return Colors.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView textview;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textview = itemView.findViewById(R.id.color);
+        }
+    }
 }
+
+
